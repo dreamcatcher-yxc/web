a23b94f76c685471da8e9043ab3e0c90aa8e6485
dreamcatcher-yxc <1905719625@qq.com>
2017-10-27 00:32:19 +0800

    ES6 函数可变参数
    Signed-off-by: dreamcatcher-yxc <1905719625@qq.com>

--- es6/methodTest.js
+++ es6/methodTest.js
@@ -3,3 +3,39 @@ const prompt = (msg = '', num = 20) => {
   //console.log(num);
   console.log(`${'*'.repeat(num)}${msg}${'*'.repeat(num)}`);
 }
+
+prompt('函数作用域');
+{
+    let foo = 'outer';
+    function bar(func = () => foo) {
+      let foo = 'bar';
+      console.log(func()); // outer, 因为此时 foo 指向为函数外部定义的 foo 变量.
+    }
+    bar();
+
+    // 下面是一个比较复杂的例子
+
+}
+
+prompt('应用');
+{
+  function throwIfMissing() {
+    throw new Error('Missing parameter');
+  }
+  function foo(mustParameter = throwIfMissing()) {
+    console.log(mustParameter);
+  }
+  foo(1);
+  //foo();
+}
+
+prompt('2. 项数组中插入数字');
+{
+    function appendValsToArr(arr, ...nums) {
+      nums.forEach(num => arr.push(num));
+    }
+    let arr = [1, 2, 3];
+    console.log(arr.join(','));
+    appendValsToArr(arr, 4, 5, 6);
+    console.log(arr.join(','));
+}
