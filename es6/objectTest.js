38476ce8a465f9e83f7e257c6142f265e01fa873
yangxiuchu <yangxiuchu@gznb.com>
2017-10-31 19:56:01 +0800

    Object 扩展学习

--- es6/objectTest.js
+++ es6/objectTest.js
@@ -99,4 +99,53 @@ prompt('Object.assign');
   let s = {b : 3, c : 4};
   Object.assign(t, s);
   console.log(t);
+  // 对象转换
+  let tObj = Object.assign(2); // [Number : 2]
+  console.log(tObj);
+  // undefined 和 null 无法转换为对象.
+  //Object.assign(null); // 报错
+  //Object.assign(undefined); // 报错
+
+  //let obj = {a: 1};
+  //Object.assign(obj, undefined) === obj // true
+  //Object.assign(obj, null) === obj // true
+
+  console.log(Object(2)); // [Number : 2]
+  console.log(Object(true)); // [Boolean : true]
+  console.log(Object('abc')); // [String : 'abc']
+
+  // 注意点:
+  // 1. Object.assign 使用的拷贝方式为浅拷贝.
+  let obj1 = {a : {b : 1}};
+  let obj2 = Object.assign({}, obj1);
+  obj1.a.b = 2;
+  console.log(obj2.a.b); // 2
+
+  // 2. Object.assign 进行的是同名属性的替换.
+
+  // 3. 数组的处理
+  // 相当于以数组的下标为 key 惊醒拷贝.
+  console.log(Object.assign([1, 2, 3], [4, 5])); // [4, 5, 3]
+
+  // 4. 取值的处理
+  // Object.assign 只能复制值, 如果需要复制的值为一个取值函数, 则先将取值函数的值返回之后在将返回的值复制.
+  let source = {
+    get foo() {return 1}
+  };
+  console.log(Object.assign({}, source)); // {foo : 1}
+}
+
+prompt('Object.assign 的常见用途', 40);
+prompt('1. 为对象添加属性');
+prompt('2. 为对象添加方法');
+prompt('3. 克隆对象');
+prompt('4. 合并多个对象');
+prompt('5. 为对象制定默认值');
+{
+
+}
+
+prompt('对象的可枚举性与遍历');
+{
+  
 }
